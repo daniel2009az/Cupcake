804c650a547aa2df170bf7218ba2d605e393b1e6
daniel2009az <daniel2009az@gmail.com>
2024-11-26 12:59:05 -0300

    change sttetings

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ python -m pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --email with --noinput
+python manage.py createsuperuser --noinput
