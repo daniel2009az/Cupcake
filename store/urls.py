84f5d4234a911f55ab460da81949f2c9d78f2e51
daniel2009az <daniel2009az@gmail.com>
2024-11-28 14:37:26 -0300

    change sttetings

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static 
 
 
 urlpatterns = [
@@ -12,4 +13,4 @@ urlpatterns = [
     
     path('new/', views.new, name='new'),
 
-]
+]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
