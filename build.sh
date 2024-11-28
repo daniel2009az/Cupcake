fff6cf01316e9c9b4c65e1b9ac28552e4eaad15a
daniel2009az <daniel2009az@gmail.com>
2024-11-27 21:45:19 -0300

    change sttetings

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 python -m pip install -r requirements.txt
+python -m pip install psycopg2-binary
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
