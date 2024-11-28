bfecf53256efc0241a2eb2eaf553d127db963910
daniel2009az <daniel2009az@gmail.com>
2024-11-28 00:04:55 -0300

    change sttetings

--- build.sh
+++ build.sh
@@ -7,4 +7,6 @@ python -m pip install psycopg2-binary
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
-python manage.py createsuperuser --noinput --if-not-exists
+if ! python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(is_superuser=True).exists())" | grep -q "True"; then
+    python manage.py createsuperuser --no-input
+fi
