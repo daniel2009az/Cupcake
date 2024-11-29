107c02e0730ff226dd8152b3ddf15c2e09130400
daniel2009az <daniel2009az@gmail.com>
2024-11-28 21:38:56 -0300

    change sttetings

--- ecom/urls.py
+++ ecom/urls.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 from django.urls import path, include
 from . import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
 ] 
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
