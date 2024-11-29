cfb89842fd8f1375dd95d7cb026768ef2b3ecd2f
daniel2009az <daniel2009az@gmail.com>
2024-11-29 17:32:04 -0300

    change sttetings

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-# mkdir -p /opt/render/project/src/media/uploads/products -p || true
-# cp -R media/uploads/products/*  /opt/render/project/src/media/uploads/products/ -p || true 
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
@@ -8,6 +6,8 @@ python -m pip install -r requirements.txt
 python -m pip install psycopg2-binary
 python manage.py makemigrations
 python manage.py migrate
+mkdir -p /opt/render/project/src/media/uploads/products -p || true
+cp -R media/uploads/products/*  /opt/render/project/src/media/uploads/products/ -p || true 
 python manage.py collectstatic --no-input
 if ! python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(is_superuser=True).exists())" | grep -q "True"; then
     python manage.py createsuperuser --no-input
