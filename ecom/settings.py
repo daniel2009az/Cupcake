7570f7b13bce71f97d7db6a9fcaf7da4465248a2
daniel2009az <daniel2009az@gmail.com>
2024-11-28 21:05:27 -0300

    change sttetings

--- ecom/settings.py
+++ ecom/settings.py
@@ -112,6 +112,8 @@ STATICFILES_DIRS = ['static/']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
 
 
 
