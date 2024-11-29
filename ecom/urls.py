8e880ad0e2e1d470e830f82bf3f47f172cedd587
daniel2009az <daniel2009az@gmail.com>
2024-11-29 04:09:39 -0300

    change sttetings

--- ecom/urls.py
+++ ecom/urls.py
@@ -9,8 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
-] 
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]   +   static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
