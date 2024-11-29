7d52eb0a89181841ab0cec094d902188e6647746
daniel2009az <daniel2009az@gmail.com>
2024-11-29 18:21:02 -0300

    change sttetings

--- store/models.py
+++ store/models.py
@@ -1,12 +1,7 @@
-import os
 from typing import Any
 from django.db import models
 import datetime
-from django.core.files.storage import default_storage
 from django.core.exceptions import ValidationError
-from django.utils.html import mark_safe
-from django.db.models.signals import post_delete
-from django.dispatch import receiver
 
 
 class Category(models.Model):
@@ -47,39 +42,10 @@ class Product(models.Model):
     is_discount = models.BooleanField(default=False)
     sale_price = models.DecimalField(default=0, max_digits=10, decimal_places=2, blank=True, null=True)
     date = models.DateField(default=datetime.datetime.today, editable=True)
-    persistent_image = models.CharField(max_length=255, blank=True, null=True)
-
-    def save(self, *args, **kwargs):
-        # Se uma nova imagem foi carregada
-        if self.image:
-            # Gera um nome de arquivo único
-            filename = f'uploads/products/{self.name}_{self.id}_{self.image.name}'
-            
-            # Salva a imagem no storage
-            saved_path = default_storage.save(filename, self.image)
-            
-            # Atualiza o campo persistent_image com o caminho salvo
-            self.persistent_image = saved_path
-            
-            # Limpa o campo de imagem original após salvar
-            self.image = None
-
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return  self.name
 
-@receiver(post_delete, sender=Product)
-def delete_product_image(sender, instance, **kwargs):
-    # Tenta deletar a imagem persistente
-    if instance.persistent_image:
-        try:
-            default_storage.delete(instance.persistent_image)
-        except Exception as e:
-            print(f"Erro ao deletar imagem: {e}")
-
-    
-    
     
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
